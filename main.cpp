011ef44026449ebd07621d4a8a2739ed567db573
ndrisanseep3 <33136014+ndrisanseep3@users.noreply.github.com>
2024-10-11 17:10:30 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1, num2, add;
+    
+    // Asking for input
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    
+    // Addition two numbers
+    add = num1 + num2;
+    
+    // Display output
+    cout << "The addition of two numbers = " << add << endl;
+    return 0;
+}
